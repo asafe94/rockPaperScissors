fd67508e7eedc702e26da0917d75c19436997719
Patrick Asafe <patrickasafe@gmail.com>
2022-01-08 11:56:46 -0300

    commit

--- script.js
+++ script.js
@@ -1,61 +1,100 @@
-//function computerPlay 
+let playerOptLocked;
+let computerOptLocked;
+let winnerOptLocked;
+let loserOptLocked;
+let result;
 
-function playRound() {
-    
-    let playerSelection = prompt('Please, choose between: Rock, Paper and Scissors.');
-    let computerSelection = Math.floor(Math.random()* 3);
-    let roundResult;
-    let wtfhappened;
+//function chose random between 0-2 and interpret this as rock = 0, paper = 1 and scissors = 2.
+let computerOptChoser = (computerOption = Math.floor(Math.random()* 3)) => {
+	switch (computerOption) {
+		case 0:
+			computerOption = 'rock';
+			computerOptLocked = computerOption;
+			return computerOptLocked;
+		case 1:
+			computerOption = 'paper';
+			computerOptLocked = computerOption;
+			return computerOptLocked;
+		case 2:
+			computerOption = 'scissors';
+			computerOptLocked = computerOption;
+			return computerOptLocked;
+}};
+//function that asks the player an option, set it to lowercase, store the playerOption as playerOptLocked ,  also give an error alert if an invalid option is chosen.
+let playerOptChoser = (playerOption) => {
+	playerOption = prompt('Please, choose between: Rock, Paper and Scissors.').toLocaleLowerCase();
+	if (playerOption === ('rock') || ('paper') || ('scissors')) {
+		playerOptLocked = playerOption; 
+		return playerOption;
+	} else {
+		alert('Please, select a valid option.');
+}}
+//function that select a result for player option vs computer option
+let roundResult = (playerOption , computerOption = computerOptChoser()) => {
+	if (playerOption === computerOption) {
+		result = 'tie';
+		return result;
+	} else if ((playerOption === 'rock' && computerOption === 'scissors')
+		|| (playerOption === 'scissors' && computerOption === 'paper')
+		|| (playerOption === 'paper' && computerOption === 'rock')) {
+		winnerOptLocked = playerOptLocked;
+		loserOptLocked = computerOptLocked;
+		result = 'You won!';
+		return result;
+	} else {
+		winnerOptLocked = computerOptLocked;
+		loserOptLocked = playerOptLocked;
+		result = 'I won!';
+		return result;
+}} 
 
-    playerSelection = playerSelection.toLowerCase()
-        
-    switch(playerSelection) {
-        case 'rock':
-            playerSelection = 2;
-            break;
-        case 'paper':
-            playerSelection = 4;
-            break;
-        case 'scissors':
-            playerSelection = 8;
-            break;
-    }
-    switch(computerSelection) {
-        case 0:
-            computerSelection = -2;
-            break;
-        case 1:
-            computerSelection = -4;
-            break;
-        case 2:
-            computerSelection = -8;
-            break;
-    }
-    
-    roundResult = (playerSelection + computerSelection)
-    
-    //trying to debug below why i'm only winning???
-    
-    alert(playerSelection)
-    alert(computerSelection)
-    alert(roundResult)
 
-    let x = roundResult
-    switch(true) {
-        case (x = 0):
-            roundResult = 'Even';
-            break;
-        case (2 <= x <= 4):
-        case (x = -6):
-            roundResult = 'You won!';
-            break;
-        case (-4 <= x <= -2):
-        case (x = 6):
-            roundResult = 'You lost!';
-            break;
-            
-    }
-    message = roundResult
-    alert(message)
 
+
+let loserOption = (winnerOption = winnerOptLocked) => {
+	switch(winnerOption){
+		case 'rock':
+			return 'scissors'
+		case 'scissors':
+			return 'paper'
+		case 'paper':
+			return 'rock'
+}}
+
+
+let resultCondition = (winnerOption = winnerOptLocked) => {
+	switch (winnerOption) {
+		case 'rock':
+			return 'blunts'
+		case 'paper':
+			return 'wraps'
+		case 'scissors':
+			return 'cuts'
+}}
+
+
+let fnlMsg = (playerOption = playerOptLocked, computerOption = computerOptLocked,
+	 winnerOption = winnerOptLocked, loserOption = loserOptLocked, finalResult = result) => {
+	let msg = "You have chosen the power of " + playerOption + 
+	".\n I have chosen the power of " + computerOption + 
+	".\n The logic's gods have spoken: " + winnerOption + " " + resultCondition(winnerOptLocked) + " " + loserOption + 
+	".\n" + finalResult
+		switch(result) {
+			case 'You won!':
+			case 'I won!':
+				console.log("obvious")
+				return msg
+			case 'tie':
+				msg = "You have chosen the power of " + playerOption + 
+				".\n I have chosen the power of " + computerOption + 
+				".\n The logic's gods have spoken: " + playerOption + " ties against " + computerOption + 
+				".\n It's a " + finalResult + "."
+				console.log("obvious")
+				return msg
+}}
+
+//function that play the game using functions to input playerOption and computerOption
+let playRound = (playerOption = playerOptChoser(), computerOption = computerOptChoser()) => {
+	result = roundResult(playerOption, computerOption)
+	return fnlMsg(playerOption, computerOption, winnerOptLocked, loserOptLocked, result)
 }
